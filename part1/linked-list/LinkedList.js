ccb9277b86fe0d95280c7e1776c243ce69b6ba5e
Sunny Sinhababu <sunnysinhababu@gmail.com>
2026-01-05 18:53:43 +0530

    Part1: Linked List pop functionality

--- part1/linked-list/LinkedList.js
+++ part1/linked-list/LinkedList.js
@@ -24,9 +24,39 @@ class LinkedList {
     return this;
   }
   // pop
-  pop() {}
+  pop() {
+    if (!this.head) return undefined;
+    let pre = this.head;
+    let temp = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
+  }
 }
 
 const linkedList = new LinkedList(4);
+console.log("before push ");
+linkedList.log();
 linkedList.push(5);
+linkedList.push(6);
+linkedList.push(7);
+console.log("after push ");
+linkedList.log();
+console.log("before pop ");
+console.log(linkedList.pop());
+console.log(linkedList.pop());
+console.log(linkedList.pop());
+console.log(linkedList.pop());
+console.log(linkedList.pop());
+console.log(linkedList.pop());
+console.log("after pop ");
 linkedList.log();
